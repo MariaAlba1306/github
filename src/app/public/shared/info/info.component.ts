6f1e855f745ee3e1ce127095ec5e3753f8a57409
MariaAlba1306 <maria.alba@wallapop.com>
2022-12-08 12:44:00 +0100

    created info component

--- src/app/public/shared/info/info.component.ts
+++ src/app/public/shared/info/info.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
-  styleUrls: ['./info.component.css']
+  styleUrls: ['./info.component.css'],
 })
-export class InfoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class InfoComponent {
+  repos: number = 110;
+  gists: number = 40;
+  followers: number = 3240;
+  following: number = 210;
 }

72e10a9f85cab7cecba6778adfc8e6645592b9ad
MariaAlba1306 <maria.alba@wallapop.com>
2022-12-08 11:13:53 +0100

    profile created

--- src/app/public/features/components/repository/profile/profile.component.ts
+++ src/app/public/features/components/repository/profile/profile.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ProfileComponent {
+  name: string = 'María alba';
+  userName: string = 'María';
+  location: string = '';
+  email: string = '';
+  blogLink: string = '';
+  year: number = 0;
+  image: string =
+    'https://www.saramusico.com/wp-content/uploads/2019/04/foto-de-perfil-en-linkedin.jpg';
 }

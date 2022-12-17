805fe54d6f6afd95a4d2b45a6e97dd4828e16737
MariaAlba1306 <maria.alba@wallapop.com>
2022-12-17 19:02:19 +0100

    previous values deleted

--- src/app/public/features/components/repository/profile/profile.component.ts
+++ src/app/public/features/components/repository/profile/profile.component.ts
@@ -16,12 +16,4 @@ export class ProfileComponent {
       'https://www.saramusico.com/wp-content/uploads/2019/04/foto-de-perfil-en-linkedin.jpg',
     html_url: '',
   };
-  name: string = '';
-  userName: string = '';
-  location: string = '';
-  email: string = '';
-  blogLink: string = '';
-  year: number = 0;
-  image: string =
-    'https://www.saramusico.com/wp-content/uploads/2019/04/foto-de-perfil-en-linkedin.jpg';
 }

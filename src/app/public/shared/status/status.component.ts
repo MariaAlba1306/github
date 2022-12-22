01d04d3671bc0b5a2e1187687b057344e6ec52b3
MariaAlba1306 <maria.alba@wallapop.com>
2022-12-22 18:52:12 +0100

    status cards added

--- src/app/public/shared/status/status.component.ts
+++ src/app/public/shared/status/status.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-status',
   templateUrl: './status.component.html',
-  styleUrls: ['./status.component.css']
+  styleUrls: ['./status.component.css'],
 })
-export class StatusComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class StatusComponent {}

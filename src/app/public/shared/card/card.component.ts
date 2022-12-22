93f7524ae665c69b7532ff85a5f47ea8a0700a67
MariaAlba1306 <maria.alba@wallapop.com>
2022-12-22 18:44:09 +0100

    card repository profile created and styles changed

--- src/app/public/shared/card/card.component.ts
+++ src/app/public/shared/card/card.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css'],
 })
-export class CardComponent {}
+export class CardComponent {
+  mockRepostoriesInfo = {
+    name: 'name',
+    html_url: '',
+    description: 'description',
+    forks: '',
+    watchers: '',
+  };
+}
